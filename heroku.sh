c5ca2610aa62c5c915e8cf36394ed0f92c7b4c4a
Yasu <a1214yk@aiit.ac.jp>
2013-08-02 18:58:17 +0900

    push heroku

---
+++ heroku.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo TRAVIS_BRANCH=${TRAVIS_BRANCH}
+case ${TRAVIS_BRANCH} in
+    master)
+  echo "it's Master!"
+    wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+  git remote add heroku git@heroku.com:weekly-report.git
+  echo "Host heroku.com" >> ~/.ssh/config
+  echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+  echo "   CheckHostIP no" >> ~/.ssh/config
+  echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+  heroku keys:clear
+  yes | heroku keys:add
+  yes | git push -f heroku master
+  heroku pg:reset DATABASE --confirm weekly-report
+  heroku run rake db:migrate
+  heroku run rake db:seed
+  heroku restart  
+  ;;
+
+    *)
+  echo "it's Other."
+  exit 0
+  ;;
+esac
